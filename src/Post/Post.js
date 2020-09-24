1503f0cd8f7c77608b2163f472752ff4d49a06fc
Ivan <ivan.lakhanin@gmail.com>
2020-09-24 14:50:03 +0300

    Styling Post

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import styled from './Post.module.css'
+import Avatar from '@material-ui/core/Avatar'
 
 function Post() {
     return (
-        <div>
-            <h3>Username</h3>
-            {/* header-> avatar + username */}
+        <div className={styled.post}>
+            <div className={styled.header}>
+                <Avatar
+                className={styled.avatar}
+                alt='Username'
+                src='/static/images/avatar/1.jpg'
+                />
+                <h3>Username</h3>
+            </div>
 
-            <img src='https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png'/>
-            {/* image */}
+            <img className={styled.image} src='https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png'/>
 
 
-            <h4>Username: caption</h4>
-            {/* username + caption */}
+            <h4 className={styled.text}><strong>Username: </strong>caption</h4>
         </div>
     )
 }
