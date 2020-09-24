a1d0be38fbd19cd95ab45b4d08c544fe914372fd
Ivan <ivan.lakhanin@gmail.com>
2020-09-24 14:14:21 +0300

    Header styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styled.app}>
+      <div className={styled.header}>
+        <img
+         className={styled.headerImage}
+         src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQp6HPVhiV-Y8A3dplMSqyJz32F2B-QMKn09A&usqp=CAU'
+         alt=''
+         />
+      </div>
+
+      {/* Header */}
+
+      {/* Post */}
+      {/* Post */}
+      
     </div>
   );
 }
