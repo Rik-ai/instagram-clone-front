27132747c4645f2310df4ca170675c01acdbac02
Ivan <ivan.lakhanin@gmail.com>
2020-09-24 15:47:23 +0300

    Add image, username, caption to Post

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -2,22 +2,19 @@ import React from 'react'
 import styled from './Post.module.css'
 import Avatar from '@material-ui/core/Avatar'
 
-function Post() {
+function Post(props) {
     return (
         <div className={styled.post}>
             <div className={styled.header}>
                 <Avatar
                 className={styled.avatar}
                 alt='Username'
-                src='/static/images/avatar/1.jpg'
+                src=''
                 />
-                <h3>Username</h3>
+                <h3>{props.username}</h3>
             </div>
-
-            <img className={styled.image} src='https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png'/>
-
-
-            <h4 className={styled.text}><strong>Username: </strong>caption</h4>
+            <img className={styled.image} src={props.imageUrl}/>
+            <h4 className={styled.text}><strong>{props.username}: </strong>{props.caption}</h4>
         </div>
     )
 }
